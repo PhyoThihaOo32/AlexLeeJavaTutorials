4e6706e76598782968648f9e5849877124ed712c
PhyoThihaOo <phyothihaoo.pto@gmail.com>
2024-09-04 10:11:44 -0400

    Initial Commit

---
+++ src/com/AlexLeeJavaPractices/EscapeCharacters.java
@@ -0,0 +1,16 @@
+package com.AlexLeeJavaPractices;
+
+public class EscapeCharacters {
+	
+	public static void main(String[]args) {
+		
+		System.out.println("This is \"fish\" swimming in the river");
+		System.out.println("how many double quotes are there \"\"\"\"");
+		System.out.println("This is a backslash \\");
+		System.out.println("What do you mean \"Grummpy\"?");
+		System.out.println("The file is located at ~\\\\Destop\\EpicSkins");
+		System.out.println("This is a tab \t <!");
+		System.out.println("so \nmany \nlines");
+	}
+
+}
